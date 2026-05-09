6f8a57423d92e1acb5b3bb1a6697655e626a9dc6
Kevin Chen <chenkevin1119@gmail.com>
2026-05-08 23:50:19 -0400

    updated db file

--- db/database.js
+++ db/database.js
@@ -1,7 +1,7 @@
 const Database = require('better-sqlite3');
 const path = require('path');
 
-const db = new Database(path.join(__dirname, '..', 'quiz.db'));
+const db = new Database('/app/data/quiz.db');
 
 // Enable WAL mode for better performance
 db.pragma('journal_mode = WAL');
