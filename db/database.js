56c9dfb22bc6ad6d1b6957b32c21f6ca97b52f98
Kevin Chen <chenkevin1119@gmail.com>
2026-05-09 00:16:58 -0400

    fix: move db to /app/db to avoid volume conflict

--- db/database.js
+++ db/database.js
@@ -1,7 +1,7 @@
 const Database = require('better-sqlite3');
 const path = require('path');
 
-const db = new Database('/app/data/quiz.db');
+const db = new Database('/app/db/quiz.db');
 
 // Enable WAL mode for better performance
 db.pragma('journal_mode = WAL');
